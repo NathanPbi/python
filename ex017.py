7b46bfbc5dc12bfa0c09742148bd6080e2ef1e65
NathanPbi <natanpbi@hotmail.com>
2020-09-06 15:08:24 -0300

    Add exercices

--- ex017.py
+++ ex017.py
@@ -1,3 +1,9 @@
-salario = float(input('Digite o valor do salário: R$ '))
-novo = salario + salario * 15 / 100
-print('Com base no atual salário de R$ {:.2f}, seu novo salário com 15% de aumento será R$ {:.2f}'.format(salario, novo))
+n = int(input('Digite um número: '))
+# d = n * 2
+# t = n * 3
+# r = n ** (1/2)
+print('O dobro de {} vale {}'.format(n, n*2))
+print('O triplo de {} vale {}. \nA raiz quadrada de {} vale {:.2f}.'. format(n, (n*3), n, (n**(1/2))))
+
+
+# Outra forma de calcular raiz quadrada é utilizando o pow Ex.: pow(n, (1/2))
---
+++ ex018.py
@@ -0,0 +1,3 @@
+nota1 = float(input('Primeira nota do aluno: '))
+nota2 = float(input('Segunda nota do aluno: ')) 
+print('A media entre {} e {} é igual a {}'.format(nota1, nota2, nota1 + nota2/2))
