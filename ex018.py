c319b628c351546297a9c5d2e242f4fb5b4a65b9
NathanPbi <natanpbi@hotmail.com>
2020-09-06 15:21:47 -0300

    Alteracao

--- ex018.py
+++ ex018.py
@@ -1,3 +1,3 @@
 nota1 = float(input('Primeira nota do aluno: '))
 nota2 = float(input('Segunda nota do aluno: ')) 
-print('A media entre {} e {} é igual a {}'.format(nota1, nota2, nota1 + nota2/2))
+print('A media entre {} e {} é igual a {}'.format(nota1, nota2, (nota1 + nota2)/2))
